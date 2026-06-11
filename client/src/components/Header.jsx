b7f23d0bd31b8209dd6693901638aae789caa289
Manajit Saha <manajitsaha49@gmail.com>
2026-06-11 23:21:22 +0530

    Complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,30 @@
 import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import {useSelector} from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 function Header() {
     const { currentUser } = useSelector((state) => state.user);
-    console.log(currentUser);
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const urlParams = new URLSearchParams(window.location.search);
+
+        urlParams.set('searchTerm', searchTerm);
+
+        navigate(`/search?${urlParams.toString()}`);
+    };
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl);
+        }
+    }, []);
+
     return (
         <header className='bg-slate-200 shadow-md'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -15,11 +35,16 @@ function Header() {
                     </h1>
                 </Link>
 
-                <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                <form
+                    onSubmit={handleSubmit}
+                    className='bg-slate-100 p-3 rounded-lg flex items-center'
+                >
                     <input
                         type='text'
                         placeholder='Search...'
                         className='bg-transparent focus:outline-none w-24 sm:w-64'
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
                     />
 
                     <button type='submit'>
